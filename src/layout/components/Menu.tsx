d9f5109143fa3d87eefb029a10672962efff5372
JiHyoung You <fbwlgud51@gmail.com>
2026-09-19 22:04:42 +0900

    design: 메뉴 애니메이션 수정

--- src/layout/components/Menu.tsx
+++ src/layout/components/Menu.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { AnimatePresence, motion } from "motion/react";
 import { Link } from "react-router";
 
@@ -14,6 +15,8 @@ const menuItems = [
 ];
 
 const Menu = ({ isOpen, onClose }: MenuProps) => {
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
   return (
     <AnimatePresence>
       {isOpen && (
@@ -21,7 +24,7 @@ const Menu = ({ isOpen, onClose }: MenuProps) => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          className="fixed top-20 inset-x-0 bottom-0 z-50
+          className="fixed top-20 inset-x-0 bottom-24 z-50
                     flex flex-col items-center justify-center gap-5
                     "
         >
@@ -29,19 +32,27 @@ const Menu = ({ isOpen, onClose }: MenuProps) => {
             <motion.div
               key={item.path}
               initial={{ opacity: 0, y: 10 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ delay: i * 0.05 }}
+              animate={{
+                opacity: hoveredIndex === null || hoveredIndex === i ? 1 : 0.2,
+                y: 0,
+              }}
+              transition={{
+                opacity: { duration: 0.3 },
+                y: { delay: i * 0.05 },
+              }}
+              onMouseEnter={() => setHoveredIndex(i)}
+              onMouseLeave={() => setHoveredIndex(null)}
             >
               <Link
                 to={item.path}
                 onClick={onClose}
                 className="flex gap-2 group"
               >
-                <span className="font-sans mt-5">[0{i + 1}]</span>
+                <span className="font-sans mt-1">[0{i + 1}]</span>
                 <span className="relative inline-grid h-24 [perspective:1000px]">
                   <div className="absolute inset-0 -z-10 bg-[url('/images/highlight.png')] bg-contain bg-center bg-no-repeat opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
 
-                  <div className="col-start-1 row-start-1 grid grid-cols-1 grid-rows-1 [transform-style:preserve-3d] transition-transform duration-500 ease-out group-hover:[transform:rotateX(180deg)]">
+                  <div className="h-24 col-start-1 row-start-1 grid grid-cols-1 grid-rows-1 [transform-style:preserve-3d] transition-transform duration-500 ease-out group-hover:[transform:rotateX(180deg)]">
                     <span
                       className="col-start-1 row-start-1 place-self-center [backface-visibility:hidden]
                                     font-sans text-8xl font-extralight whitespace-nowrap tracking-tight"
